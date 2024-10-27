e2c21aa2e46e946550cdb2892378911322462c01
1155159410 <1155159410@link.cuhk.edu.hk>
2024-10-27 13:56:37 +0800

    Update main.py
    - Add weight decay to the SGD optimizer.
    - Update the scheduler.

--- main.py
+++ main.py
@@ -339,8 +339,8 @@ def loss_func(output_batch, label_batch):
 
 
 # %% Define the optimizer and learning rate scheduler
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10)
 
 
 # %% Helper function to count the number of correct predictions in a batch
