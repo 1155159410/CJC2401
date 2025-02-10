65592022036262e7255884aec7ebf00d8796008f
gordon0907 <gordonhui0907@gmail.com>
2025-02-10 18:39:24 +0800

    Update main_v2.py
    - Organize the code in a better way.
    - Decrease queue size from 30 to 1, as a longer queue means a higher delay.

--- main_v2.py
+++ main_v2.py
@@ -280,9 +280,58 @@ class FrameProcessor:
             )
 
 
-# %% TODO
+# %% Version 1: Sequential, no multithreading (~13 FPS)
+def v1():
+    # Open the default camera
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        raise RuntimeError("Cannot open webcam")
+
+    posture_system = PostureCorrectionSystem()
+    frame_times = []
+
+    while cv2.waitKey(1) != ord('q'):  # Up to 1000 loops per second
+        ret, frame = cap.read()  # Read a frame
+        if not ret:  # If frame read fails
+            print("Failed to get frame")
+            break
+
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        capture_time = time.time()
+
+        result: dict = posture_system.process_image(rgb_frame)
+
+        frame_processor = FrameProcessor(rgb_frame)
+        if (keypoints := result.get('keypoints')) is not None:
+            frame_processor.draw_skeletons(keypoints)
+        rgb_frame = frame_processor.rgb_frame
+
+        bgr_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("Posture Correction System", bgr_frame)
+
+        current_time = time.time()
+        while frame_times and frame_times[0] < current_time - 1:
+            frame_times.pop(0)
+        frame_times.append(capture_time)
+        fps = len(frame_times)
+
+        print("Delay", current_time - capture_time)
+        print("FPS", fps)
+        print()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    posture_system.stop_thread()
+    print("System stopped")
+    del posture_system
+    print("System deleted")
+
+
+# %% Version 2: Multithreading (~27 FPS)
 def v2():
-    in_queue: queue.Queue[dict] = queue.Queue(maxsize=30)
+    in_queue: queue.Queue[dict] = queue.Queue(maxsize=1)  # Limit to at most 1 pending frame
     out_queue: queue.Queue[dict] = queue.Queue()
 
     camera = Camera()
@@ -321,8 +370,12 @@ def v2():
     print("System stopped")
     del posture_system
     print("System deleted")
-    quit()
 
 
+# %% Version 3: Multithreading + down sampling (~X FPS)
+...
+
+# %% Entry point
 if __name__ == "__main__":
+    v1()
     v2()
